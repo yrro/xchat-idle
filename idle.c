0d2dc931627a609a19d78d12a338c385b6f32b3a
Sam Morris <sam@robots.org.uk>
2015-03-15 15:56:04 +0000

    Version 1.0

--- idle.c
+++ idle.c
@@ -1,15 +1,20 @@
 /**
- * Idle. Plugin to set /away automatically.
- * Copyright 2004 Sam Morris
+ * idle: XChat plugin that sets /away automatically.
+ * Copyright 2004-2007 Sam Morris <sam@robots.org.uk>
  *
- * This plugin is distributed under the terms of the GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
  *
- * Version 0.3
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * To compile:
- *   gcc -Wall -shared -o idle.so idle.c -L/usr/X11R6/lib -lX11 -lXext -lXss
- *
- * TODO: dealloc mit_info on plugin deinit?
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
  */
 
 #include <xchat/xchat-plugin.h>
@@ -21,23 +26,23 @@
 #include <stdio.h>
 
 #ifdef DEBUG
-	#define debug_print(string) xchat_print(ph, string)
-	#define debug_printf(string, ...) xchat_printf(ph, string, ...)
+    #define debug_print(string) xchat_print(ph, string)
+    #define debug_printf(string, ...) xchat_printf(ph, string, ...)
 #else
-	#define debug_print(string)
-	#define debug_printf(string, ...)
+    #define debug_print(string)
+    #define debug_printf(string, ...)
 #endif
 
 /* Settings */
 static const unsigned long idleThreshold = 60 * 5; // in seconds
-static const char * awayText = "Idle for 5 minutes.";
+static const char* awayText = "Idle for >= 5 minutes.";
 
 /* Plugin data */
-static xchat_plugin *ph;
+static xchat_plugin* ph;
 
-static Display *display = NULL;
+static Display* display = NULL;
 static Window window = 0;
-static XScreenSaverInfo *mit_info;
+static XScreenSaverInfo* mit_info;
 static int event_base, error_base;
 
 // RFC2812 Sec 2.3: max command line length 512 characters including CRLF
@@ -49,10 +54,9 @@ static char awayCommand[awayCommandLength];
 /**
  * Sends a command to every server that we are connected to.
  */
-static void send_command(xchat_plugin *ph, const char *command)
-{
-	xchat_command(ph, command);
-	debug_print("command sent");
+static void send_command(xchat_plugin* ph, const char* command) {
+    xchat_command(ph, command);
+    debug_print("command sent");
 }
 
 /**
@@ -60,77 +64,72 @@ static void send_command(xchat_plugin *ph, const char *command)
  *
  * NB: assumes the away status is synchronised between different servers.
  */
-static int checkTimeout(void *userdata)
-{
-	const char * awayinfo = xchat_get_info(ph, "away");
+static int checkTimeout(void* userdata) {
+    const char* awayinfo = xchat_get_info(ph, "away");
 
-	XScreenSaverQueryInfo(display, window, mit_info);
+    XScreenSaverQueryInfo(display, window, mit_info);
 
-	debug_printf("Idle time = %u ms.", mit_info->idle);
-	
-	if (mit_info->idle > idleThreshold * 1000)
-	{
-		if (awayinfo == NULL)
-		{
-			debug_print("Going away!");
-			send_command(ph, awayCommand);
-		}
-		else
-		{
-			debug_print("already away.");
-		}
-	}
-	else
-	{
-		if (awayinfo != NULL)
-		{
-			// Only set back if we set away in the first place
-			if (strncmp(awayinfo, awayText, strlen(awayText)) == 0)
-			{
-				debug_print("we're back.");
-				send_command(ph, "allserv away");
-			}
-		}
-		else
-		{
-			debug_print("still back.");
-		}
-	}
+    debug_printf("Idle time = %u ms.", mit_info->idle);
 
-	return 1;
+    if (mit_info->idle > idleThreshold * 1000) {
+        if (awayinfo == NULL) {
+            debug_print("Going away!");
+            send_command(ph, awayCommand);
+        } else {
+            debug_print("already away.");
+        }
+    } else {
+        if (awayinfo != NULL) {
+            // Only set back if we set away in the first place
+            if (strncmp(awayinfo, awayText, strlen(awayText)) == 0) {
+                debug_print("we're back.");
+                send_command(ph, "allserv back");
+            }
+        } else {
+            debug_print("still back.");
+        }
+    }
+
+    return 1;
 }
 
-int xchat_plugin_init(xchat_plugin *plugin_handle,
-	char **plugin_name,
-	char **plugin_desc,
-	char **plugin_version,
-	char **arg)
-{
-	ph = plugin_handle;
-
-	*plugin_name = "idle";
-	*plugin_desc = "sets /away automatically";
-	*plugin_version = "0.1";
-
-	snprintf(awayCommand, awayCommandLength, "allserv away %s", awayText);
-
-	display = XOpenDisplay(NULL);
-	if (display == NULL)
-		return 0;
-	
-	window = DefaultRootWindow(display);
-	if (window == 0)
-		return 0;
-
-	if (! XScreenSaverQueryExtension(display, &event_base, &error_base))
-		return 0;
-
-	mit_info = XScreenSaverAllocInfo();
-
-	xchat_hook_timer(ph, 10 * 1000, checkTimeout, NULL);
-	//xchat_hook_command(ph, "CHECKTIMEOUT", XCHAT_PRI_NORM, (void *)checkTimeout, "blah", NULL);
-
-	xchat_print(ph, "idle plugin loaded.");
-
-	return 1;
+int xchat_plugin_deinit(void) {
+    XFree(mit_info);
 }
+
+int xchat_plugin_init(
+    xchat_plugin* plugin_handle,
+    char** plugin_name,
+    char** plugin_desc,
+    char** plugin_version,
+    char** arg
+) {
+    ph = plugin_handle;
+
+    *plugin_name = "idle";
+    *plugin_desc = "sets /away automatically";
+    *plugin_version = "0.1";
+
+    snprintf(awayCommand, awayCommandLength, "allserv away %s", awayText);
+
+    display = XOpenDisplay(NULL);
+    if (display == NULL)
+        return 0;
+
+    window = DefaultRootWindow(display);
+    if (window == 0)
+        return 0;
+
+    if (!XScreenSaverQueryExtension(display, &event_base, &error_base))
+        return 0;
+
+    mit_info = XScreenSaverAllocInfo();
+
+    xchat_hook_timer(ph, 10 * 1000, checkTimeout, NULL);
+
+    xchat_print(ph, "idle plugin loaded.");
+
+    return 1;
+}
+
+// vim: ts=8 sts=4 sw=4 et
